8de68066434ed64ec667d3a0888643b66f2b3861
Your Name <141213@qq.com>
2019-03-25 09:16:00 +0800

    实现了获取用户基本信息

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # 接入指南
 
-分支：
+分支：jieruzhinan
+
+# access_token
+  https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPIDsecret=APPSECRET
+需要三个参数:
+  grant_type: client_credential
+  appid:      wx1c9bf5bbd5d400bf
+  secret:     f369de3ac73c34bcf4a7f0739b351516
+
+  https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx1c9bf5bbd5d400bf&secret=f369de3ac73c34bcf4a7f0739b351516
+
+  19_ykQM5aekyrIQXEL9EZYjFzPIvE9jX8f_oDkNkQlvHpBRjy2YkPcyXqvGs3oRSaOPkuV087pX5vdcj63LbrD1XJnx291EoXdZChdXWPl_R887eDBT6iVbA_NLL6P26fYHZaSknWwer5NacFmKRYVbAJAAHE
+
+# 实现获取粉丝基本信息
+
+1. 需要调用微信提供的一个接口
+  https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
+需要三个参数:
+  access_token：微信开发时一个很重要很重要的东西，叫做接口凭证
+  openid:       直接在FromUserName
+  lang
+
+http://thirdwx.qlogo.cn/mmopen/kqBrZeNmdiaYfns4Ehz6MhSGD9bbI4ROL6RZHASKibjoicJMhicgP7CUEM9CDiaJkicurf2vmFqbrrQC3LQ0X8Uv1RMJENGR1dLowa/132
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const wechat = require('wechat');
+const https = require('https');
 const app = express();
 
 const config = {
   token: 'weixin',
-  appid: 'wx0e41ae3cd6988f29',
+  appid: 'wx1c9bf5bbd5d400bf',
   encodingAESKey: 'FBGVi4gxVcEYFdvjwL9kTPLPL8T3MpJlkqoaSwi0dR3',
   checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
@@ -20,6 +21,28 @@ app.use('/wechat', wechat(config, (req, res, next) => {
   }
   else if(message.Content === '我爱你'){
     res.reply('我也爱你');
+    return;
+  }
+
+  // 如果用户输入获取我的基本信息
+  if (message.Content === '基本信息') {
+    // 使用 获取用户基本信息的接口获取当前用户的基本信息
+    let accToKen = '19_ykQM5aekyrIQXEL9EZYjFzPIvE9jX8f_oDkNkQlvHpBRjy2YkPcyXqvGs3oRSaOPkuV087pX5vdcj63LbrD1XJnx291EoXdZChdXWPl_R887eDBT6iVbA_NLL6P26fYHZaSknWwer5NacFmKRYVbAJAAHE';
+    let openid = message.FromUserName;
+    let url = `
+    https://api.weixin.qq.com/cgi-bin/user/info?access_token=${accToKen}&openid=${openid}&lang=zh_CN
+    `
+    https.get(url, (response) => {
+      let buf = '';
+      response.on('data', (chunk) => {
+        buf += chunk;
+      })
+      response.on('end', () => {
+        console.log(buf);
+        res.reply(buf);
+      })
+    })
+
     return;
   }
 
